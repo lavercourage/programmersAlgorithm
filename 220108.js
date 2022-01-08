8f3a8e54837c81fc415f40b5ae613ba8cf57dfda
lavercourage <smqkemfht@gmail.com>
2022-01-08 23:51:40 +0900

    220108 직사각형별찍기

---
+++ 220108.js
@@ -0,0 +1,20 @@
+// 문제 주소
+// 직사각형 별찍기
+// https://programmers.co.kr/learn/courses/30/lessons/12969?language=javascript
+
+// 문제 접근 방법
+
+// 코드
+process.stdin.setEncoding("utf8");
+process.stdin.on("data", (data) => {
+  const n = data.split(" ");
+  const a = Number(n[0]),
+    b = Number(n[1]);
+  for (let i = 0; i < b; i++) {
+    let result = "";
+    for (let j = 0; j < a; j++) {
+      result = result + "*";
+    }
+    console.log(result);
+  }
+});
