d580d2dffed7fd9d049dae92c2573ef90f14ad71
lavercourage <smqkemfht@gmail.com>
2022-02-19 23:42:02 +0900

    220219 제일작은수

---
+++ 220219_제일작은수제거하기.js
@@ -0,0 +1,21 @@
+// 문제 주소
+// 제일 작은 수 제거하기
+// https://programmers.co.kr/learn/courses/30/lessons/12935
+
+// 코드
+function solution(arr) {
+  // 제일 작은 수를 저장해주는 변수
+  let min = arr[0];
+  for (let i = 1; i < arr.length; i++) {
+    if (min > arr[i]) {
+      min = arr[i];
+    }
+  }
+
+  arr.splice(arr.indexOf(min), 1);
+  if (arr.length === 0) {
+    return [-1];
+  }
+
+  return arr;
+}
